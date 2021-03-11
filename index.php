d32eafbef13bddf5378846d9e0a2be5d522bda95
BIVILLE Allan <biviall@ndlp.fr>
2021-03-11 15:04:36 +0100

    Cool

--- index.php
+++ index.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>FORMULAIRE DE CONTACT POUR ME CONTACTER !</title>
+    <title>FORMULAIRE DE CONTACT</title>
 </head>
 <body align=center>
+
 <fieldset>
-<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-    Name : <input type="text" name="fname">
+<form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
+    <h1>T ki frere ?</h1>
+    Name : <br/><input type="text" name="name"><br/>
+    Email : <br/><input type="text" name="email"><br/>
+    WebSite : <br/><input type="text" name="website"><br/>
+    Comment : <br/><textarea name="comment" rows="5" cols="40"></textarea><br/>
     <br/><br/>
+    <h1>c koi ton SAIIIIXSE ??????</h1>
+    <input type="radio" name="gender" value="female">Female<br/>
+    <input type="radio" name="gender" value="male">Male<br/>
+    <input type="radio" name="gender" value="Sebastien">Sebastien<br/>
+    <input type="radio" name="gender" value="other">Other<br/>
+
     <input type="submit"/>
 </form>
 </fieldset>
     <?php
+    
     if  ($_SERVER["REQUEST_METHOD"] == "POST"){
-        $name = $_REQUEST['fname'];
+        $name = $_REQUEST['name'];
         if (empty($name)){
             echo "! Name is empty !";
         } else{
@@ -25,9 +37,16 @@
         }
     }
 
-
-
-    /*
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // collect value of input field
+        echo 'name = '.$_POST['name'] . '<br/>';
+        echo 'email = '.$_POST['email'] . '<br/>';
+        echo 'website = '.$_POST['website'] . '<br/>';
+        echo 'comment = '.$_POST['comment'] . '<br/>';
+        echo 'gender = '.$_POST['gender'] . '<br/>';
+    }
+/*
+    
     echo 'Le programme demarre....';
     $x = 75;
     $p = ' zocolat';
